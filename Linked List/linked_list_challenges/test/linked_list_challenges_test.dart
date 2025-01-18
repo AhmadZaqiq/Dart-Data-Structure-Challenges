2f5ae20da14fc5629691574826164f4e7446cd80
Ahmad Zaqiq <160167174+AhmadZaqiq@users.noreply.github.com>
2025-01-19 00:29:08 +0200

    Delete Linked List/linked_list_challenges directory

--- Linked List/linked_list_challenges/test/linked_list_challenges_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:linked_list_challenges/linked_list_challenges.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
