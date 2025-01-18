9c6b5ffdf9eca50ed17eaa755acee0b107fcc4f9
Ahmad Zaqiq <160167174+AhmadZaqiq@users.noreply.github.com>
2025-01-19 00:22:50 +0200

    Add files via upload

---
+++ data_structure/Stacks/challenge1_reverse_a_list/test/challenge1_reverse_a_list_test.dart
@@ -0,0 +1,8 @@
+import 'package:challenge1_reverse_a_list/challenge1_reverse_a_list.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
---
+++ data_structure/Stacks/challenge2_balance_the_parentheses/test/challenge2_balance_the_parentheses_test.dart
@@ -0,0 +1,8 @@
+import 'package:challenge2_balance_the_parentheses/challenge2_balance_the_parentheses.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
